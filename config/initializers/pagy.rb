7b25231b0313db0b977a6fdf49256d3d316b5ea4
Writtek <fci@FCIs-MacBook-Pro.local>
2026-04-23 13:05:52 +0530

    Improve Pagy pagination UI with custom Tailwind CSS styling

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,4 +1,6 @@
 require "pagy/extras/overflow"
+require "pagy/extras/size"
 
 Pagy::DEFAULT[:items] = 20
 Pagy::DEFAULT[:overflow] = :last_page
+Pagy::DEFAULT[:size] = [ 1, 2, 2, 1 ] # [outer, before, after, outer]
